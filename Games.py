3e4072358706b96706a9aa12e47cc37ea7a46abc
Carolan <carolana@lancs.local>
2017-12-08 17:53:38 +0000

    Added games to new function file Games and added a division game

---
+++ Games.py
@@ -0,0 +1,265 @@
+import random
+
+def addition():
+	check = True
+	difficulty = 4
+	diffCheck = 0
+	while(check == True): 
+		#Generate the question values at random between 1 & the difficulty level
+		A = random.randint(1, difficulty)
+		B = random.randint(1, difficulty)
+		
+		#Output the question and verify the input is a valid int
+		inputCheck = False
+		while(inputCheck == False):
+			try:
+				print("\nWhat is ", A, " + ", B, "?")
+				inputValue = int(input())
+				inputCheck = True
+			except ValueError:
+				print("The provided input was not a number, please try agian!")
+			
+		#Verify that the input is the correct answer
+		if inputValue == (A + B):
+			if difficulty < 10:
+				diffCheck += 1
+			print("That is correct, well done!")
+		else:
+			print("\nNot right, the correct answer is:", (A+B))
+			if difficulty < 4:
+				diffCheck -= 1
+			
+		#Adjust the difficulty level if required
+		if diffCheck <= -3:
+			if difficulty > 4:
+				difficulty -= 1
+			diffCheck = 0
+		elif diffCheck >= 3:
+			difficulty += 1
+			diffCheck = 0
+		
+		#Check if the user would like to continue - Loop back if yes - exit if no -
+		while(True):
+			userCheck = input("Press Y to try another sum or N to stop.\n")
+			if userCheck in ('Y', 'y'):
+				break
+			elif userCheck in ('N', 'n'):
+				check = False
+				break
+						
+def subtraction():
+	check = True
+	difficulty = 4
+	diffCheck = 0
+	while(check == True): 
+		#Generate the question values at random between 1 & the difficulty level
+		A = random.randint(1, difficulty)
+		B = random.randint(1, difficulty)
+		
+		#Ensure the result is positive 
+		while(A-B < 0):
+			A = random.randint(1, difficulty)
+			B = random.randint(1, difficulty)
+		
+		#Output the question and verify the answer is a valid int
+		inputCheck = False
+		while(inputCheck == False):
+			try:
+				print("\nWhat is ", A, " - ", B, "?")
+				inputValue = int(input())
+				inputCheck = True
+			except ValueError:
+				print("The provided input was not a number, please try agian!")
+			
+		#Verify that the input is the correct answer
+		if inputValue == (A - B):
+			if difficulty < 10:
+				diffCheck += 1
+			print("That is correct, well done!")
+		else:
+			print("\nNot right, the correct answer is:", (A-B))
+			if difficulty < 4:
+				diffCheck -= 1
+				
+		#Adjust the difficulty level if required
+		if diffCheck <= -3:
+			if difficulty > 4:
+				difficulty -= 1
+			diffCheck = 0
+		elif diffCheck >= 3:
+			difficulty += 1
+			diffCheck = 0
+		
+		#Check if the user would like to continue - Loop back if yes - exit if no -	
+		while(True):
+			userCheck = input("Press Y to try another sum or N to stop.\n")
+			if userCheck in ('Y', 'y'):
+				break
+			elif userCheck in ('N', 'n'):
+				check = False
+				break
+				
+def multiplication():
+	check = True
+	difficulty = 4
+	diffCheck = 0
+	while(check == True):
+		#Generate the question values at random between 1 & the difficulty level		
+		A = random.randint(1, difficulty)
+		B = random.randint(1, difficulty)
+		
+		#Output the question and verify the answer is a valid int
+		inputCheck = False
+		while(inputCheck == False):
+			try:
+				print("\nWhat is ", A, " x ", B, "?")
+				inputValue = int(input())
+				inputCheck = True
+			except ValueError:
+				print("The provided input was not a number, please try agian!")
+			
+		#Verify that the input is the correct answer
+		if inputValue == (A * B):
+			if difficulty < 10:
+				diffCheck += 1
+			print("That is correct, well done!")
+		else:
+			print("\nNot right, the correct answer is:", (A*B))
+			if difficulty < 4:
+				diffCheck -= 1
+
+		#Adjust the difficulty level if required
+		if diffCheck <= -3:
+			if difficulty > 4:
+				difficulty -= 1
+			diffCheck = 0
+		elif diffCheck >= 3:
+			difficulty += 1
+			diffCheck = 0
+			
+		#Check if the user would like to continue - Loop back if yes - exit if no -
+		while(True):
+			userCheck = input("Press Y to try another sum or N to stop.\n")
+			if userCheck in ('Y', 'y'):
+				break
+			elif userCheck in ('N', 'n'):
+				check = False
+				break
+				
+def division():
+	check = True
+	difficulty = 4
+	diffCheck = 0
+	while(check == True): 
+		#Generate the question values at random between 1 & the difficulty level
+		A = random.randint(1, difficulty)
+		B = random.randint(1, difficulty)
+		
+		#Ensure the result is positive 
+		while(A%B != 0):
+			A = random.randint(1, difficulty)
+			B = random.randint(1, difficulty)
+		
+		#Output the question and verify the answer is a valid int
+		inputCheck = False
+		while(inputCheck == False):
+			try:
+				print("\nWhat is ", A, " / ", B, "?")
+				inputValue = int(input())
+				inputCheck = True
+			except ValueError:
+				print("The provided input was not a number, please try agian!")
+			
+		#Verify that the input is the correct answer
+		if inputValue == (A/B):
+			if difficulty < 10:
+				diffCheck += 1
+			print("That is correct, well done!")
+		else:
+			print("\nNot right, the correct answer is:", (A/B))
+			if difficulty < 4:
+				diffCheck -= 1
+				
+		#Adjust the difficulty level if required
+		if diffCheck <= -3:
+			if difficulty > 4:
+				difficulty -= 1
+			diffCheck = 0
+		elif diffCheck >= 3:
+			difficulty += 1
+			diffCheck = 0
+		
+		#Check if the user would like to continue - Loop back if yes - exit if no -	
+		while(True):
+			userCheck = input("Press Y to try another sum or N to stop.\n")
+			if userCheck in ('Y', 'y'):
+				break
+			elif userCheck in ('N', 'n'):
+				check = False
+				break
+				
+def mixture():
+	check = True
+	difficulty = 4
+	diffCheck = 0
+	while(check == True):
+		#Generate the question values at random between 1 & the difficulty level		
+		A = random.randint(1, difficulty)
+		B = random.randint(1, difficulty)
+		C = random.randint(1, 4)
+		
+		while((C == 2 and A - B < 0) or (C == 4 and A % B > 0)):
+			A = random.randint(1, difficulty)
+			B = random.randint(1, difficulty)
+			
+		result = mixResult(A,B,C)
+		
+		#Output the question and verify the answer is a valid int
+		inputCheck = False
+		while(inputCheck == False):
+			try:
+				print("\nWhat is ", A, " / ", B, "?")
+				inputValue = int(input())
+				inputCheck = True
+			except ValueError:
+				print("The provided input was not a number, please try agian!")
+			
+		#Verify that the input is the correct answer
+		if inputValue == (result):
+			if difficulty < 10:
+				diffCheck += 1
+			print("That is correct, well done!")
+		else:
+			print("\nNot right, the correct answer is:", result)
+			if difficulty < 4:
+				diffCheck -= 1
+
+		#Adjust the difficulty level if required
+		if diffCheck <= -3:
+			if difficulty > 4:
+				difficulty -= 1
+			diffCheck = 0
+		elif diffCheck >= 3:
+			difficulty += 1
+			diffCheck = 0
+			
+		#Check if the user would like to continue - Loop back if yes - exit if no -
+		while(True):
+			userCheck = input("Press Y to try another sum or N to stop.\n")
+			if userCheck in ('Y', 'y'):
+				break
+			elif userCheck in ('N', 'n'):
+				check = False
+				break
+				
+def mixResult(A,B,C):
+	
+	#Calculate the result based on the type of calculation
+	if C == 1:
+		return A + B
+	elif C == 2:
+		return A - B
+	elif C == 3:
+		return A * B
+	elif C == 4:
+		return A / B
--- MathsGame.py
+++ MathsGame.py
@@ -1,121 +1,25 @@
 import random
+from Games import addition, subtraction, multiplication, division
 
 #Take and store the inital user selection input
 inputNumber = input("Select one of the following options:\n(1) Addition (2) Subtraction (3) Multiplication (4) Division (5) Random sums (6) Quit\n")
 
 #Run the game while the exit command has not been selected, also handles unexpected inputs
 while (inputNumber != '6'):
-	
-	#Reset the game values to their inital state
-	difficulty = 4
-	check = True
-	
+
 	#Run the game selected by the user
 	#ADDITION
 	if inputNumber == '1':
-		while(check == True): 
-			#Generate the question values at random between 1 & the difficulty level
-			A = random.randint(1, difficulty)
-			B = random.randint(1, difficulty)
-			
-			#Output the question and verify the input is a valid int
-			inputCheck = False
-			while(inputCheck == False):
-				try:
-					print("\nWhat is ", A, " + ", B, "?")
-					inputValue = int(input())
-					inputCheck = True
-				except ValueError:
-					print("The provided input was not a number, please try agian!")
-				
-			#Verify that the input is the correct answer
-			#If so the increase the dificulty by 1 to max of 10
-			if inputValue == (A + B):
-				if difficulty < 10:
-					difficulty += 1
-				print("That is correct, well done!")
-			else:
-				print("\nNot right, the correct answer is:", (A+B))
-			
-			#Check if the user would like to continue - Loop back if yes - exit if no -
-			while(True):
-				userCheck = input("Press Y to try another sum or N to stop.\n")
-				if userCheck in ('Y', 'y'):
-					break
-				elif userCheck in ('N', 'n'):
-					check = False
-					break
+		addition()
 	#SUBTRACTION
 	elif inputNumber == '2':
-		while(check == True): 
-			#Generate the question values at random between 1 & the difficulty level
-			A = random.randint(1, difficulty)
-			B = random.randint(1, difficulty)
-			
-			#Output the question and verify the answer is a valid int
-			inputCheck = False
-			while(inputCheck == False):
-				try:
-					print("\nWhat is ", A, " - ", B, "?")
-					inputValue = int(input())
-					inputCheck = True
-				except ValueError:
-					print("The provided input was not a number, please try agian!")
-				
-			#Verify that the input is the correct answer
-			#If so the increase the dificulty by 1 to max of 10
-			if inputValue == (A - B):
-				if difficulty < 10:
-					difficulty += 1
-				print("That is correct, well done!")
-			else:
-				print("\nNot right, the correct answer is:", (A-B))
-				
-			#Check if the user would like to continue - Loop back if yes - exit if no -	
-			while(True):
-				userCheck = input("Press Y to try another sum or N to stop.\n")
-				if userCheck in ('Y', 'y'):
-					break
-				elif userCheck in ('N', 'n'):
-					check = False
-					break
+		subtraction()
 	#MULTIPLICATION
 	elif inputNumber == '3':
-		while(check == True):
-			#Generate the question values at random between 1 & the difficulty level		
-			A = random.randint(1, difficulty)
-			B = random.randint(1, difficulty)
-			
-			#Output the question and verify the answer is a valid int
-			inputCheck = False
-			while(inputCheck == False):
-				try:
-					print("\nWhat is ", A, " x ", B, "?")
-					inputValue = int(input())
-					inputCheck = True
-				except ValueError:
-					print("The provided input was not a number, please try agian!")
-				
-			#Verify that the input is the correct answer
-			#If so the increase the dificulty by 1 to max of 10
-			if inputValue == (A * B):
-				if difficulty < 10:
-					difficulty += 1
-				print("That is correct, well done!")
-			else:
-				print("\nNot right, the correct answer is:", (A*B))
-				
-			#Check if the user would like to continue - Loop back if yes - exit if no -
-			while(True):
-				userCheck = input("Press Y to try another sum or N to stop.\n")
-				if userCheck in ('Y', 'y'):
-					break
-				elif userCheck in ('N', 'n'):
-					check = False
-					break
+		multiplication()
 	#DIVISION
 	elif inputNumber == '4':
-		print("Four")
+		division()
 	#MIXTURE
 	elif inputNumber == '5':
 		print("Five!")
